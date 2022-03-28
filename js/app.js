23acea69cb37a72d4b1600df3f6852b604478cd0
Duy Nguyen <duynghoang05@gmail.com>
2022-03-28 23:57:04 +0700

    retain color when resizing. Add reset btn

--- js/app.js
+++ js/app.js
@@ -1,23 +1,70 @@
 
-function main() {
+function colorCanvas() {
+    let redSlider = document.querySelector('.rgb-slider-red');
+    let greenSlider = document.querySelector('.rgb-slider-green');
+    let blueSlider = document.querySelector('.rgb-slider-blue');
+
+    let minRGB = 0;
+    let maxRGB = 255;
+    let defaultRGB = Math.floor((maxRGB - minRGB)/2);
+
+    setRGBSliderSettings(redSlider, minRGB, maxRGB, defaultRGB);
+    setRGBSliderSettings(greenSlider, minRGB, maxRGB, defaultRGB);
+    setRGBSliderSettings(blueSlider, minRGB, maxRGB, defaultRGB);
+
+    let redColor = redSlider.value;
+    let greenColor = greenSlider.value;
+    let blueColor = blueSlider.value;
+    let rgbDisplay = document.querySelector('.rgb-display')
+
+    redSlider.addEventListener('input', () => {
+        redColor = redSlider.value;
+        rgbDisplay.style['background-color'] = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
+    });
+
+    greenSlider.addEventListener('input', () => {
+        greenColor = greenSlider.value;
+        rgbDisplay.style['background-color'] = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
+    });
+
+    blueSlider.addEventListener('input', () => {
+        blueColor = blueSlider.value;
+        rgbDisplay.style['background-color'] = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
+    });
+
+    let mouseDown;
+    window.addEventListener('mousedown', () => { mouseDown = true; });
+    window.addEventListener('mouseup', () => { mouseDown = false; });
+
+    let allGridItems = document.getElementsByClassName('grid-item');
+    Array.from(allGridItems).forEach(gridItem => gridItem.addEventListener('mouseover', (e) => {
+        if (mouseDown) {
+            e.target.style['background-color'] = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
+        }
+    }));
+}
+
+
+function setRGBSliderSettings(slider, minRGB, maxRGB, defaultRGB) {
+    slider.setAttribute('min', minRGB);
+    slider.setAttribute('max', maxRGB);
+    slider.setAttribute('value', defaultRGB);
+}
+
+
+function getCanvas() {
     let mainContainer = document.querySelector('.main-container');
     let sizeNumSlider = document.querySelector('.size-num-slider')
     let sizeNumSliderDisplay = document.querySelector('.slider-display')
-    let sideNum;
+    let resetButton = document.querySelector('.slider-reset')
+    let sideSize = 500;
 
     let minSizeNum = 10;
     let maxSizeNum = 100;
     let defaultSizeNum = Math.floor((maxSizeNum - minSizeNum)/2);
 
-    let rgbDisplay = document.querySelector('.rgb-display')
-    let minRGB = 0;
-    let maxRGB = 255;
-    let defaultRGB = Math.floor((maxRGB - minRGB)/2);
-    let redColor = defaultRGB;
-    let greenColor = defaultRGB; 
-    let blueColor = defaultRGB;
-
-    getCanvas(mainContainer, defaultSizeNum, redColor, greenColor, blueColor);
+    mainContainer.appendChild(getGridItems(defaultSizeNum, sideSize));
+    colorCanvas();
 
     sizeNumSlider.setAttribute('min', minSizeNum);
     sizeNumSlider.setAttribute('max', maxSizeNum);
@@ -26,62 +73,25 @@ function main() {
     sizeNumSliderDisplay.textContent = `${defaultSizeNum} x ${defaultSizeNum}`;
 
     sizeNumSlider.addEventListener('input', () => {
-        let sliderVal = sizeNumSlider.value
+        let sliderVal = sizeNumSlider.value;
         sizeNumSliderDisplay.textContent = `${sliderVal} x ${sliderVal}`;
-    })
-
-    let redSlider = document.querySelector('.rgb-slider-red')
-    let greenSlider = document.querySelector('.rgb-slider-green')
-    let blueSlider = document.querySelector('.rgb-slider-blue')
-
-    redSlider.setAttribute('min', minRGB);
-    redSlider.setAttribute('max', maxRGB);
-    redSlider.setAttribute('value', defaultRGB);
-
-    greenSlider.setAttribute('min', minRGB);
-    greenSlider.setAttribute('max', maxRGB);
-    greenSlider.setAttribute('value', defaultRGB);
-
-    blueSlider.setAttribute('min', minRGB);
-    blueSlider.setAttribute('max', maxRGB);
-    blueSlider.setAttribute('value', defaultRGB);
-
-    redSlider.addEventListener('input', () => {
-        redColor = redSlider.value;
-        rgbDisplay.style['background-color'] = `rgb(${redColor}, ${greenColor}, ${blueColor})`
-    })
-
-    greenSlider.addEventListener('input', () => {
-        greenColor = greenSlider.value;
-        rgbDisplay.style['background-color'] = `rgb(${redColor}, ${greenColor}, ${blueColor})`
-    })
-        
-    blueSlider.addEventListener('input', () => {
-        blueColor = blueSlider.value;
-        rgbDisplay.style['background-color'] = `rgb(${redColor}, ${greenColor}, ${blueColor})`
-    })
+    });
 
     sizeNumSlider.addEventListener('change', () => {
-        sideNum = sizeNumSlider.value;
+        let sideNum = sizeNumSlider.value;
         mainContainer.removeChild(mainContainer.lastElementChild);
-        getCanvas(mainContainer, defaultSizeNum, redColor, greenColor, blueColor);
-    })
+        mainContainer.appendChild(getGridItems(sideNum, sideSize));
+        colorCanvas();
+    });
+
+    resetButton.addEventListener('click', () => {
+        let sideNum = sizeNumSlider.value;
+        mainContainer.removeChild(mainContainer.lastElementChild);
+        mainContainer.appendChild(getGridItems(sideNum, sideSize));
+        colorCanvas();
+    });
 }
 
-function getCanvas(mainContainer, sideNum, redColor, greenColor, blueColor) {
-    let sideSize = 500;
-    let mouseDown;
-    window.addEventListener('mousedown', () => {mouseDown = true})
-    window.addEventListener('mouseup', () => {mouseDown = false})
-
-    mainContainer.appendChild(getGridItems(sideNum, sideSize));
-    let allGridItems = document.getElementsByClassName('grid-item');
-    Array.from(allGridItems).forEach(gridItem => gridItem.addEventListener('mouseover', (e) => {
-        if (mouseDown) {
-            e.target.style['background-color'] = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
-        }
-    }))
-}
 
 function getGridItems(sideNum, sideSize) {
     let mainDiv = document.createElement('div');
@@ -108,5 +118,5 @@ function getGridItems(sideNum, sideSize) {
 
 
 window.onload = () => {
-    main()
+    getCanvas()
   }
